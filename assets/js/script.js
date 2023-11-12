5a6058d5df847b418de97fc3fc719099c1866b7a
Jesus M <marquez.jay444@gmail.com>
2023-11-12 02:02:29 -0600

    starts pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// select elements from dom
+  // start button
+  // word blanks div
+
+// define an array of words
+
+  // add event listener for clicks to start button
+    // get random word from words array 
+    // reset word blanks, an "_" for each letter of the new word
+    // set off timer 
+
+  
