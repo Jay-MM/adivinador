f82da872ed7add5cb71647e1636349b6035f2ac2
Jesus M <marquez.jay444@gmail.com>
2023-11-12 02:53:52 -0600

    renders underscores into DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,20 @@ let word;
 // score variable
 // timer variable
 
-// takes random word and splits it into letters
+// resets word blanks, an "_" for each letter of the new word
+  // takes random word and splits it into letters
 function renderChars(){
-  console.log(word)
+  // var to hold new string
+  let str = ""
+  for (let i = 0; i < word.length; i++){
+    // if we guessed a character
+      // add character into our str
+    // else
+      // take the current value of str and add "_"
+      str += '_ '
+  }
+  // set textContent of wordBLanks to be str and trim all extra whitespace
+  wordBlanks.textContent = str.trim()
 }
 
 function startRound(){
@@ -23,11 +34,12 @@ function startRound(){
 const randomIndex = Math.floor(Math.random() * words.length)
   // uses the random index in the words array to access a random item in array and assigns it to the global word variable
   word = (words[randomIndex])
-// resets word blanks, an "_" for each letter of the new word
 // sets off timer 
   renderChars()
  }
   // adds event listener for clicks to start button
   startButton.addEventListener('click', startRound)
+
+  
 
   
